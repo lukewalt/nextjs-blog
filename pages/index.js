d1c86bac352be93fbbc0cbf3a70b5387202353cd
Luke Woodward <luke.wdwrd@gmail.com>
2017-08-27 17:18:10 -0500

    created dynamic elements on entry file

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,23 @@
 import Link from 'next/link';
 import Layout from '../comps/Layout'
 
-const Index = () => (
-  <Layout>
-    <p style={{textAlign: 'center'}}>Hello Next JS</p>
-  </Layout>
+
+const postLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 )
 
-export default Index
+
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is great" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>
+)
