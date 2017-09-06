416ba555f40309f37da616c0eebb77b9a6fb105f
Luke Woodward <luke.wdwrd@gmail.com>
2017-09-06 15:38:29 -0500

    installed next redux wrapper; created store

--- .next/bundles/pages/about.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([6],{355:function(e,t,u){e.exports=u(356)},356:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(6),o=n(l),r=u(116),a=n(r);t.default=function(){return o.default.createElement(a.default,null,o.default.createElement("h1",null,"About Page"))}}},[355]);
-            return { page: comp.default }
-          })
-        
